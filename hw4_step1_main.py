b5941b938b1a2dec783b8213fa8c07de7d7dcc58
Adam Imdieke <imdie022@umn.edu>
2025-11-26 11:25:25 -0600

    1A

--- hw4_step1_main.py
+++ hw4_step1_main.py
@@ -28,7 +28,7 @@ class Config:
         self.parser.add_argument('--schedule', type=str, default="1000", help="regular/irregular schedule to use (jumps)")
         
         # hyperparameters for algos
-        self.parser.add_argument('--ps_type', type=str, default="ILVR", help="choose from projection, DPS, DDNM")
+        self.parser.add_argument('--ps_type', type=str, default="ILVR", help="choose from unconditional, ILVR, MCG, DDNM, DPS")
         self.parser.add_argument('--degradation', type=str, default='Inpainting', help='SR or Inpainting')
         
         # hyperparameters for the inpainting mask & SR
@@ -50,8 +50,9 @@ class posterior_samplers():
     
     def predict_x0_hat(self, x_t, t, model_output):
         alpha_t = utils.extract_and_expand(self.sampler_operator.alphas_cumprod, t, x_t)
+        # x0_hat = (1/sqrt(alpha_bar_t)) * x_t - (sqrt(1-alpha_bar_t)/sqrt(alpha_bar_t)) * epsilon
         coeff1 = 1.0 / torch.sqrt(alpha_t)
-        coeff2 = torch.sqrt(1.0 - alpha_t) / alpha_t * coeff1  # simplifies to sqrt(1-alpha_t)/sqrt(alpha_t)
+        coeff2 = torch.sqrt(1.0 - alpha_t) / torch.sqrt(alpha_t)
         x0_hat = coeff1 * x_t - coeff2 * model_output 
         return utils.clip_denoised(x0_hat)
     
@@ -64,10 +65,10 @@ class posterior_samplers():
         coef2 = utils.extract_and_expand(posterior_mean_coef2, t, model_output)
         x_t_prev = coef1 * x0_hat + coef2 * model_output
 
-
         noise = torch.randn_like(x_t)
-        if t != 0:
-            x_t_prev += utils.extract_and_expand(sigma, t, noise) * noise
+        # t is a tensor, so we need to check if any element is non-zero
+        if t.any():
+            x_t_prev = x_t_prev + utils.extract_and_expand(sigma, t, noise) * noise
         return x_t_prev
     
     def q_sample(self, data, t):
@@ -95,23 +96,24 @@ class posterior_samplers():
         # A_funcs.A(), A_funcs.At(), A_funcs.A_pinv()
         ############################
         # Step 1: Get model output (predicted noise)
-        model_output = self.score_model(x_t, model_t)
-        model_output, _ = torch.split(model_output, x_t.shape[1], dim=1)
+        with torch.no_grad():
+            model_output = self.score_model(x_t, model_t)
+            model_output, _ = torch.split(model_output, x_t.shape[1], dim=1)
         
-        # Step 2: Predict x0_hat from x_t (Tweedie denoised estimate)
-        x0_hat = self.predict_x0_hat(x_t, t, model_output)
-        
-        # Step 3: DDIM sampling to get x'_{t-1}
-        x_t_prev_prime = self.sample_ddim(x_t, t, x0_hat, model_output)
-        
-        # Step 4: Get y_{t-1} by noising the measurement through q_sample
-        # y0 = A^{\dagger}(y) gives us the pseudo-inverse reconstruction
-        y0 = A_funcs.A_pinv(measurement).reshape(x_t.shape)
-        y_t_prev = self.q_sample(y0, t)
-        
-        # Step 5: ILVR update: x_{t-1} = x'_{t-1} + zeta * A^{\dagger}(y_{t-1} - A(x'_{t-1}))
-        zeta_ilvr = 1.0  # Can be tuned heuristically
-        x_t_prev = x_t_prev_prime + zeta_ilvr * A_funcs.A_pinv(A_funcs.A(y_t_prev) - A_funcs.A(x_t_prev_prime)).reshape(x_t.shape)
+            # Step 2: Predict x0_hat from x_t (Tweedie denoised estimate)
+            x0_hat = self.predict_x0_hat(x_t, t, model_output)
+            
+            # Step 3: DDIM sampling to get x'_{t-1}
+            x_t_prev_prime = self.sample_ddim(x_t, t, x0_hat, model_output)
+            
+            # Step 4: Get y_{t-1} by noising the measurement through q_sample
+            # y0 = A^{\dagger}(y) gives us the pseudo-inverse reconstruction
+            y0 = A_funcs.A_pinv(measurement).reshape(x_t.shape)
+            y_t_prev = self.q_sample(y0, t)
+            
+            # Step 5: ILVR update: x_{t-1} = x'_{t-1} + zeta * A^{\dagger}(y_{t-1} - A(x'_{t-1}))
+            zeta_ilvr = 1.0  # Can be tuned heuristically
+            x_t_prev = x_t_prev_prime + zeta_ilvr * A_funcs.A_pinv(A_funcs.A(y_t_prev) - A_funcs.A(x_t_prev_prime)).reshape(x_t.shape)
         
         return x_t_prev
     
@@ -153,14 +155,45 @@ def main():
     loader = get_dataloader(dataset, batch_size=1, num_workers=0, train=False)
     noiser = GaussianNoise(conf.sigma_y*2.0)
     A_funcs = get_degradation(conf, device)
+
+    # Unconditional sampling for part (a) - generates 5 samples and saves as grid
+    if conf.ps_type == "unconditional":
+        num_samples = 5
+        samples = []
+        for i in range(num_samples):
+            print(f'\nGenerating unconditional sample {i+1}/{num_samples}...')
+            sampler_operator = DDIM(conf)
+            x_t = utils.get_noise_x_t(device)
+            pbar = list(range(conf.desired_timesteps))[::-1]
+            time_map = sampler_operator.recreate_alphas().to(device)
+            ps_ops = posterior_samplers(conf, sampler_operator, score_model)
+            
+            for idx in tqdm(pbar):
+                time = torch.tensor([idx] * x_t.shape[0], device=device)
+                # Get model output (predicted noise)
+                with torch.no_grad():
+                    model_output = score_model(x_t, time_map[time])
+                    model_output, _ = torch.split(model_output, x_t.shape[1], dim=1)
+                # Predict x0_hat and sample x_{t-1}
+                x0_hat = ps_ops.predict_x0_hat(x_t, time, model_output)
+                x_t = ps_ops.sample_ddim(x_t, time, x0_hat, model_output)
+            
+            samples.append(utils.clear_color(x_t))
+            # Save individual sample
+            sample_path = os.path.join(conf.out_path, conf.dataset, 'unconditional', f'sample_{i+1}.png')
+            os.makedirs(os.path.dirname(sample_path), exist_ok=True)
+            plt.imsave(sample_path, samples[-1])
+        return
     
-    # Sampling
+    # Sampling (for conditional methods: ILVR, MCG, DDNM, DPS)
     for i, ref_img in enumerate(loader):
         print(f'\nSampling for Image {i+1} has started!')
         sampler_operator = DDIM(conf)
         ref_img = ref_img.to(device)
         measurement = noiser(A_funcs.A(ref_img))
-        x_t = utils.get_noise_x_t(device).requires_grad_()
+        x_t = utils.get_noise_x_t(device)
+        if conf.ps_type == "DPS":  # Only DPS needs gradients
+            x_t = x_t.requires_grad_()
         pbar = (list(range(conf.desired_timesteps))[::-1])
         time_map = sampler_operator.recreate_alphas().to(device)
         ps_ops = posterior_samplers(conf, sampler_operator, score_model)
